33c1176b6113ae9a11b907884e7ffcb47596dfb3
gane0528 <mariodaisuki77@gmail.com>
2020-03-05 17:47:03 +0900

    update

--- ble2.py
+++ ble2.py
@@ -28,28 +28,20 @@ except:
 blescan.hci_le_set_scan_parameters(sock)
 blescan.hci_enable_le_scan(sock)
 
-tmp = datetime.datetime.now()
-dt = datetime.datetime(tmp.year, tmp.month, tmp.day, tmp.hour, tmp.minute, tmp.second+10)
-
-while True:
-    if datetime.datetime.now() >= dt:
-        break
 time_sta = time.time()
-print "start"
 
 while True:
     elapsed_time = time.time() - time_sta
-    if elapsed_time >= 30:
+    if elapsed_time >= 300:
         write_file(datalist)
         print "finish"
         break
-    start_time = datetime.datetime.now()+datetime.timedelta(milliseconds=100)
-    if datetime.datetime.now() >=  start_time:
-        returnedList = blescan.parse_events(sock, 10)
-        for beacon in returnedList:
-            if beacon.split(',')[1] == 'e7d61ea3f8dd49c88f2ff2484c07acb9' and beacon.split(',')[2] == '1' and beacon.split(',')[3] == '81':
-                print "----------"
-                rssi = beacon.split(',')[5]
-                # print str(elapsed_time) + 's : ' + rssi + ',' + str(10**((-70 - float(rssi)) / 20))
-                nowtime = start_time.strftime('%H:%M:%S.%f')
-                datalist.append(str(nowtime) + ',' + rssi + ',' + str(10**((-70 - float(rssi)) / 20)) + '\n')
+    returnedList = blescan.parse_events(sock, 10)
+    for beacon in returnedList:
+        if beacon.split(',')[1] == 'e7d61ea3f8dd49c88f2ff2484c07acb9' and beacon.split(',')[2] == '1' and beacon.split(',')[3] == '81':
+            print "----------"
+            rssi = beacon.split(',')[5]
+            # print str(elapsed_time) + 's : ' + rssi + ',' + str(10**((-70 - float(rssi)) / 20))
+            now = datetime.datetime.now()
+            nowtime = now.strftime('%H:%M:%S.%f')
+            datalist.append(str(nowtime) + ',' + rssi + ',' + str(10**((-70 - float(rssi)) / 20)) + '\n')
