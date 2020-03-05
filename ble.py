89d9efbb533267262b334b61c3cf3111f44396b7
gane0528 <mariodaisuki77@gmail.com>
2020-03-05 17:21:45 +0900

    決められた時間ごとに記録するようにした。

--- ble.py
+++ ble.py
@@ -13,6 +13,9 @@ BleGetFlag = True
 start = 0
 datalist = []
 
+tmp = datetime.datetime.now()
+dt = datetime.datetime(tmp.year, tmp.month, tmp.day, tmp.hour, tmp.minute + 1)
+
 
 # スキャン結果をファイルに書き込む
 def write_file(data):
@@ -24,7 +27,12 @@ def write_file(data):
 
 
 def callback(bt_addr, rssi, packet, additional_info):
-    global BleGetFlag, start
+    global BleGetFlag, start, dt
+
+    while:
+        if datetime.datetime.now() >= dt:
+            break
+
     if BleGetFlag:
         start = time.time()
         BleGetFlag = False
@@ -32,17 +40,16 @@ def callback(bt_addr, rssi, packet, additional_info):
     # 経過時間を計算
     elapsed_time = int(time.time() - start)
     # 現在の年月日時間情報取得
-    now = datetime.datetime.now()
-
-    nowtime = now.strftime('%H:%M:%S.%f')
-
-    # data = nowtime + "," + str(elapsed_time) + "," + str(bt_addr) + "," + str(rssi) + "," + str(packet) + "," + str(additional_info) + "\n"
-    data = nowtime + ","  + str(rssi) + "," + str(10**((-70 - rssi) / 20)) + "\n"
-    # print("{} :: <{}, {}> {}".format(nowtime, bt_addr, rssi, packet))
-    print("{} : {}dBm, {}m".format(nowtime, rssi, 10**((-70 - rssi) / 20)))
-    datalist.append(data)
-    # print("経過時間：{}秒".format(elapsed_time))
-    # print('{}m'.format(10**((-70 - rssi) / 20)))
+    start_time = datetime.datetime.now()+datetime.timedelta(milliseconds=100)
+    if datetime.datetime.now() ==  start_time:
+        nowtime = start_time.strftime('%H:%M:%S.%f')
+        # data = nowtime + "," + str(elapsed_time) + "," + str(bt_addr) + "," + str(rssi) + "," + str(packet) + "," + str(additional_info) + "\n"
+        data = nowtime + ","  + str(rssi) + "," + str(10**((-70 - rssi) / 20)) + "\n"
+        # print("{} :: <{}, {}> {}".format(nowtime, bt_addr, rssi, packet))
+        # print("{} : {}dBm, {}m".format(nowtime, rssi, 10**((-70 - rssi) / 20)))
+        datalist.append(data)
+        # print("経過時間：{}秒".format(elapsed_time))
+        # print('{}m'.format(10**((-70 - rssi) / 20)))
 
 
 def main():
